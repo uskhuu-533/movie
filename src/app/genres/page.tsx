14bc01b22d762410ca39e250770ff760a297f1d3
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-02-12 18:08:22 +0800

    b

--- src/app/genres/page.tsx
+++ src/app/genres/page.tsx
@@ -3,14 +3,17 @@
 import Header from "@/components/Header";
 import GenrePage from "@/components/GenrePage";
 import Footer from "@/components/Footer";
+import { Suspense } from 'react'
 
 export default function Genre() {
   return (
+    <Suspense>
     <div className="flex dark:text-white text-black flex-col w-screen items-center dark:bg-[#09090B] light:bg-white gap-[30px]">
       <Header />
 
       <GenrePage />
       <Footer />
     </div>
+    </Suspense>
   );
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,13 +3,15 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import GenrePage from "@/components/GenrePage";
-
+import { Suspense } from "react";
 export default function Home() {
   return (
+    <Suspense>
     <div className="flex dark:text-white flex-col w-screen dark:bg-[#09090B] bg-white gap-[30px] ">
       <Header />
       <GenrePage />
       <Footer />
     </div>
+    </Suspense>
   );
 }
